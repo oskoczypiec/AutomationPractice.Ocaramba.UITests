54ec7093ec650e4a264254e2cc0c258b0088ee5a
askoczypiec <askoczypiec@objectivity.co.uk>
2019-11-13 20:37:35 +0100

    WIP: shopping

---
+++ AutomationPractice.Ocaramba.UITests/AutomationPractice.Ocaramba.UITests/PageObjects/ShoppingPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ocaramba;
+using Ocaramba.Extensions;
+using Ocaramba.Types;
+using OpenQA.Selenium;
+
+namespace AutomationPractice.Ocaramba.UITests.PageObjects
+{
+    class ShoppingPage : ProjectPageBase
+    {
+        private readonly ElementLocator
+            womenCategoryPage = new ElementLocator(Locator.CssSelector, ".sf-menu > li:nth-child(1)"),
+            topsCategoryChecker = new ElementLocator(Locator.CssSelector, "#center_column > #subcategories > .clearfix > li:nth-child(2)"),
+            dressItem = new ElementLocator(Locator.CssSelector, ".product_list > li:nth-child(1)"),
+            addToCart = new ElementLocator(Locator.CssSelector, ".exclusive");
+        public ShoppingPage(DriverContext driverContext) : base(driverContext)
+        {
+        }
+
+        public void BuyItem()
+        {
+            Driver.FindElement(By.CssSelector(womenCategoryPage.Value));
+
+        }
+    }
+}
